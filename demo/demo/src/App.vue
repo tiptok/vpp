3556a9a52dcb11b840a214c37111bb183344f168
tiptok <785409885@qq.com>
2022-01-06 17:43:25 +0800

    路由

---
+++ demo/demo/src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <div id="app">首页
+    <router-view />
+  </div>
+</template>
---
+++ demo/demo/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="nav">
+        <span v-for="{ key, name } in list" :key="key"  style="padding: 10px 10px">
+            <router-link :to="`/${key}`"> {{key}} {{name}} </router-link>
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data(){
+      return{
+          list:[
+            {
+              key:"1.4",
+              name:"如何触发组件的更新"
+            },
+           {
+                key: "1.5",
+                name: "合理应用计算属性和侦听器"
+            }
+        ]
+      }
+  }
+}
+</script>
